1135c85a84eb87b86e07aca7d7e54e1ece17a3cc
michaelfeng <michealzhibin@gmail.com>
2022-07-13 16:34:27 +0800

    Delete world_hello/target directory

--- world_hello/target/release/deps/world_hello-16166003da80bf55.d
+++
@@ -1,5 +0,0 @@
-/Users/michealfeng/Documents/gitproject/rust-learning/world_hello/target/release/deps/world_hello-16166003da80bf55: src/main.rs
-
-/Users/michealfeng/Documents/gitproject/rust-learning/world_hello/target/release/deps/world_hello-16166003da80bf55.d: src/main.rs
-
-src/main.rs:
